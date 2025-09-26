e29d1c39ea186bce46c716ac41a2269c7f9f8602
fvsm99 <48387747+fvsm99@users.noreply.github.com>
2025-09-26 10:14:12 -0300

    Update SistemaEscolar.java

--- CalculadoraTemperatura/src/SistemaEscolar.java
+++ CalculadoraTemperatura/src/SistemaEscolar.java
@@ -7,26 +7,21 @@ public class SistemaEscolar {
         
         System.out.println("Digite as 8 notas anuais (2 por bimestre):");
         
-        // Recebe as 8 notas
         for (int i = 0; i < 8; i++) {
             System.out.print("Nota " + (i + 1) + ": ");
             notas[i] = scanner.nextDouble();
         }
         
-        // Calcula médias bimestrais (média de 2 notas)
         double bimestre1 = (notas[0] + notas[1]) / 2;
         double bimestre2 = (notas[2] + notas[3]) / 2;
         double bimestre3 = (notas[4] + notas[5]) / 2;
         double bimestre4 = (notas[6] + notas[7]) / 2;
         
-        // Calcula médias semestrais (média de 2 bimestres)
         double semestre1 = (bimestre1 + bimestre2) / 2;
         double semestre2 = (bimestre3 + bimestre4) / 2;
         
-        // Calcula média final (média de todos os bimestres)
         double mediaFinal = (bimestre1 + bimestre2 + bimestre3 + bimestre4) / 4;
         
-        // Exibe os resultados
         System.out.println("\nResultados:");
         System.out.println("1o Bimestre: " + bimestre1);
         System.out.println("2o Bimestre: " + bimestre2);
@@ -40,4 +35,5 @@ public class SistemaEscolar {
         
         scanner.close();
     }
+
 }
